09dbd1d98828eb2c220e82042f23f650ceaf303f
xtarzyk <xtarzyk@tutanota.com>
2022-06-22 15:09:21 +0200

    added receiving data from server, messaging in progress

--- src/form.ts
+++ src/form.ts
@@ -1,8 +1,9 @@
 import { io } from 'socket.io-client'
 
 const socket = io('ws://localhost:3001')
+let roomUserData: Object = []
 
-export const createForm = () => {
+const createForm = () => {
     const $formContainer = $('<div>').addClass('chat__form-container')
     const $form = $('<form>').addClass('chat__form').appendTo($formContainer)
     const $formRoom = $('<div>').addClass('chat__form-room').appendTo($form)
@@ -18,27 +19,61 @@ export const createForm = () => {
 }
 
 const createChatRoom = (room: string) => {
-    $('<h2>').text(`Welcome to ${room}`).appendTo('.chat')
+    $('<h2>').text(`Welcome to ${room} room`).appendTo('.chat')
 
     const $messages = $('<ul>').addClass('chat__messages').appendTo('.chat')
     const $sendForm = $('<form>').addClass('chat__send-form').appendTo('.chat')
-    const $messageInput = $('<input>').addClass('chat__message-input').appendTo($sendForm)
+    const $messageInput = $('<input>').attr('type', 'text').addClass('chat__message-input').appendTo($sendForm)
     const $sendButton = $('<button>').text('Send').addClass('chat__send-message-button').appendTo($sendForm)
+
+    sendMessage($messages, $messageInput, $sendButton)
 }
 
 const joinRoom = ($roomInput: JQuery<HTMLElement>, $nameInput: JQuery<HTMLElement>, $button: JQuery<HTMLElement>) => {
-    
     $button.on('click', () => {
         const room = $roomInput.val() as string
-        if(room && $nameInput.val()) {
+        const name = $nameInput.val() as string
+
+        if(room && name) {
             socket.emit('room', room)
-            socket.emit('name', $nameInput.val())
+            socket.emit('name', name)
             $roomInput.val('')
             $nameInput.val('')
             // window.location.assign(`/room/${room}`)
-            window.history.pushState({}, '', `/room/${room}`)
+            window.history.pushState({}, '', `${room}`)
+            getRoomUserData(name, room)
             handleRoute(room)
         }
+    })
+}
+
+const getRoomUserData = (userName: string, roomName: string) => {
+    socket.emit('userName', userName)
+    socket.on('getUserData', (user) => {
+        console.log(user)
+    })
+
+    socket.emit('roomName', roomName)
+    socket.on('getRoomData', room => {
+        console.log(room)
+    })
+}
+
+const sendMessage = ($messages: JQuery<HTMLElement>, $messageInput: JQuery<HTMLElement>, $sendButton: JQuery<HTMLElement>) => {
+    $sendButton.on('click', (event) => {
+        const messageValue = $messageInput.val()
+        console.log(messageValue)
+        event.preventDefault()
+        if (messageValue) {
+            socket.emit('sendMessage', { text: messageValue })
+            $messageInput.val('')
+            console.log('Message send', messageValue)
+        }
+    })
+
+    socket.on('receiveMessage', message => {
+        console.log('receiveMessage', message)
+        $('<li>').text(message).appendTo($messages)
     })
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { io } from 'socket.io-client'
-import { createForm, handleRoute } from './form'
+import { handleRoute } from './form'
 
 const socket = io('ws://localhost:3001')
 
 // window.onpopstate = handleRoute
-const run = () => setTimeout(() => {
-  console.log('loaded!')
-  const currentPath = window.location.pathname
-  handleRoute(currentPath)}, 1000)
-
-run()
-
-socket.emit('hello from client')
-
-socket.on('hello from server', (...args) => {
-  // hmmm...???
-})
+const currentPath = window.location.pathname
+handleRoute(currentPath)
